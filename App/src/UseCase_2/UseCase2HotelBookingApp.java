c05e4c05a4ff2c09aaa75e1a734e7316455997d5
cm8595 <cm8595@srmist.edu.in>
2026-03-20 09:40:40 +0530

    bhargav added UseCase_2

---
+++ App/src/UseCase_2/UseCase2HotelBookingApp.java
@@ -0,0 +1,38 @@
+package UseCase_2;
+/**
+ * MAIN CLASS - UseCase2HotelBookingApp
+ *
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * Description:
+ * This class demonstrates room types and static availability.
+ * Room objects are created and their details are displayed.
+ *
+ * @author Chaitanya Bhargav Malakonda
+ * @version 2.1
+ */
+public class UseCase2HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        // Static availability
+        int singleRoomsAvailable = 5;
+        int doubleRoomsAvailable = 3;
+        int suiteRoomsAvailable = 2;
+
+        // Create room objects
+        Room single = new SingleRoom();
+        Room doubleRoom = new doubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Display room details and availability
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleRoomsAvailable + "\n");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomsAvailable + "\n");
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomsAvailable + "\n");
+    }
+}
