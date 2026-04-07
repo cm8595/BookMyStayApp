8463fa2bc4962f2eac059a15c4114e8dbb652908
cm8595 <cm8595@srmist.edu.in>
2026-04-07 09:44:50 +0530

    added use case 10

--- App/src/UseCase_2/SingleRoom.java
+++
@@ -1,9 +0,0 @@
-package UseCase_2;
-/**
- * Concrete class representing a Single Room.
- */
-public class SingleRoom extends Room {
-    public SingleRoom() {
-        super(1, 150, 50.0);
-    }
-}
--- App/src/UseCase_2/SuiteRoom.java
+++
@@ -1,9 +0,0 @@
-package UseCase_2;
-/**
- * Concrete class representing a Suite Room.
- */
-public class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super(4, 500, 200.0);
-    }
-}
